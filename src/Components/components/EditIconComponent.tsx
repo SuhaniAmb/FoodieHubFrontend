044da049c126ef00332db1f973ea17c108a31c2d
Suhani <suhaniamb25@gmail.com>
2026-09-05 22:33:23 +0530

    Fix EditIconComponent syntax error

--- src/Components/components/EditIconComponent.tsx
+++ src/Components/components/EditIconComponent.tsx
@@ -19,7 +19,7 @@ export default function EditIconComponent({image})
     return(
         <div className="absolute top-0 left-0 bg-blue-500 text-white p-2" >
             {iconState?<IconComponent/>:<></>}
-            <img className="w-40 h-40 ronded " src={`${serverURL}/images/${image}`} alt="Category Icon"  onMouseOver={()=>setIconState(true)} onMouseLeave={()=>setIconState(false)" /> 
+            <img className="w-40 h-40 ronded " src={`${serverURL}/images/${image}`} alt="Category Icon"  onMouseOver={()=>setIconState(true)} onMouseLeave={()=>setIconState(false)}" /> 
         </div>
     )
 }
